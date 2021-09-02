f7c667fa8943072424df100da32e3d741ea3de66
YazilimciCocuk042 <89745197+YazilimciCocuk042@users.noreply.github.com>
2021-09-02 14:35:03 +0300

    Proje dosyası ekle.

---
+++ Hava Durumu/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Hava_Durumu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.L1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.L3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.L2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderColor = System.Drawing.Color.Black;
+            this.guna2TextBox1.BorderRadius = 2;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2TextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Location = new System.Drawing.Point(13, 13);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.guna2TextBox1.PlaceholderText = "İl, Şehir, Ülke";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Size = new System.Drawing.Size(258, 42);
+            this.guna2TextBox1.TabIndex = 0;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.DisabledState.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.White;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(53, 62);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button1.TabIndex = 1;
+            this.guna2Button1.Text = "ARAMA";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(105, 222);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(70, 70);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.guna2PictureBox1.TabIndex = 4;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // L1
+            // 
+            this.L1.BackColor = System.Drawing.Color.Transparent;
+            this.L1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.L1.ForeColor = System.Drawing.Color.White;
+            this.L1.IsSelectionEnabled = false;
+            this.L1.Location = new System.Drawing.Point(99, 113);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(22, 49);
+            this.L1.TabIndex = 5;
+            this.L1.Text = "a";
+            this.L1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // L3
+            // 
+            this.L3.BackColor = System.Drawing.Color.Transparent;
+            this.L3.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.L3.ForeColor = System.Drawing.Color.White;
+            this.L3.IsSelectionEnabled = false;
+            this.L3.Location = new System.Drawing.Point(57, 310);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(22, 49);
+            this.L3.TabIndex = 6;
+            this.L3.Text = "a";
+            this.L3.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // L2
+            // 
+            this.L2.BackColor = System.Drawing.Color.Transparent;
+            this.L2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.L2.ForeColor = System.Drawing.Color.White;
+            this.L2.IsSelectionEnabled = false;
+            this.L2.Location = new System.Drawing.Point(85, 168);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(118, 34);
+            this.L2.TabIndex = 7;
+            this.L2.Text = "az bulutlu";
+            this.L2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.IsSelectionEnabled = false;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(120, 361);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(163, 23);
+            this.guna2HtmlLabel1.TabIndex = 8;
+            this.guna2HtmlLabel1.Text = "@yazilimci.cocuk042";
+            this.guna2HtmlLabel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.guna2HtmlLabel1.Click += new System.EventHandler(this.guna2HtmlLabel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.ClientSize = new System.Drawing.Size(284, 384);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hava Durumu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel L1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel L3;
+        private Guna.UI2.WinForms.Guna2HtmlLabel L2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+    }
+}
+
---
+++ Hava Durumu/Form1.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace Hava_Durumu
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public void islem1()
+        {
+            if (guna2TextBox1.Text == "")
+            {
+                MessageBox.Show("Lütfen ilk önce bir konum gir!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Form1 frm = new Form1();
+                string htmlCode = "";
+                using (WebClient client = new WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    htmlCode = client.DownloadString($"http://api.openweathermap.org/data/2.5/weather?q={guna2TextBox1.Text}&appid=c69926e5a99b43afe8808e2bac6a6be8&lang=tr");
+                }
+                dynamic stuff = JObject.Parse(htmlCode);
+
+                dynamic sicaklik = stuff.main.temp;
+                int veriSicaklik = (sicaklik - 273);
+                L1.Text = veriSicaklik.ToString() + " °C";
+                L1.Location = new Point(frm.Size.Width / 2 - L1.Size.Width / 2 - 10, 113);
+
+                dynamic picture = stuff.weather[0].icon;
+                guna2PictureBox1.Load($"http://openweathermap.org/img/w/{picture}.png");
+
+                dynamic country = stuff.sys.country;
+                dynamic name = stuff.name;
+                L3.Text = $"{name}, {country}";
+                L3.Location = new Point(frm.Size.Width / 2 - L3.Size.Width / 2 - 10, 310);
+
+                dynamic status = stuff.weather[0].description;
+                L2.Text = status;
+                L2.Location = new Point(frm.Size.Width / 2 - L2.Size.Width / 2 - 10, 168);
+
+                guna2TextBox1.Text = "";
+            }
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Thread islem = new Thread(() => islem1());
+            islem.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            L1.Text = "";
+            L2.Text = "";
+            L3.Text = "";
+        }
+
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.instagram.com/yazilimci.cocuk042/");
+        }
+    }
+}
